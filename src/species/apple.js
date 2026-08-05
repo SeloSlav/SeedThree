1655aa3d561ebaf3d099d0616f096630bca346f2
Martin <martin.erlic@gmail.com>
2026-08-05 15:30:46 +0200

    Add species-specific apple and cherry leaf materials

--- src/species/apple.js
+++ src/species/apple.js
@@ -7,7 +7,7 @@ export const apple = {
   name: 'Cultivated Apple',
   latin: 'Malus domestica',
   bark: 'apple_bark_albedo.png',
-  leaf: 'american_beech_single_albedo.png',
+  leaf: 'apple_single_albedo.png',
   biome: 'temperate',
   controls: broadleafControls,
   foliage: {
